1c004456839ba8e4f34069a06c22e1898421c000
saiyerniakhil <saiyerniakhil>
2019-05-03 22:25:55 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Tweet() {
+    return(
+        <div className='tweet'>
+            <Avatar/>
+            <div className="content">
+            <NameWithHandle/><Time/>
+            <Message/>
+            <div className="buttons">
+                <ReplyButton/>
+                <LikeButton/>
+                <RetweetButton/>
+                <MoreOptionsButton/>
+            </div>
+            </div>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Avatar() {
+    return(
+        <img src='https://gravatar.com/avatar/nothing'
+        className='avatar'
+        alt='Avatar' />    
+    
+    )
+        
+}
+
+function Message() {
+    return(
+        <div className='message'>
+        This is less than 140 characters
+        </div>    
+    ) 
+}
+
+function NameWithHandle() {
+    return(
+        <span className='name-with-handle'>
+            <span className="name">Your Name</span>
+            <span className="handle">@YourHandle</span>
+        </span>   
+    )
+}
+
+const Time = () => (
+    <span className='time'>3h Ago</span>
+)
+
+const ReplyButton = () => (
+    <i className='fa fa-reply reply-button'></i>
+)
+
+const RetweetButton = () => (
+    <i className='fa fa-retweet retweet-button'></i>
+)
+
+const LikeButton = () => (
+    <i className='fa fa-heart like-button'></i>
+)
+
+const MoreOptionsButton = () => (
+    <i className='fa fa-ellipsis-h moreoptions-button'></i>
+)
+
+ReactDOM.render(
+    <Tweet/>,
+    document.getElementById('root')
+)
